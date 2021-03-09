5bf9e2e375b7b3b0b06d0b78d08c58d113f7575e
largomst <dingrui.zhen@gmail.com>
2021-03-09 18:31:03 +0800

    将任务列表和创建任务放入新的组件中作为一个路由

--- src/App.vue
+++ src/App.vue
@@ -5,23 +5,13 @@
       @toggle-add-task="toggleAddTask"
       :showAddTask="showAddTask"
     ></Header>
-    <div v-if="showAddTask">
-      <AddTask @add-task="addTask" />
-    </div>
-    <Tasks
-      @toggle-reminder="toggleReminder"
-      @delete-task="deleteTask"
-      :tasks="tasks"
-    />
-    <router-view></router-view>
+    <router-view :showAddTask="showAddTask"></router-view>
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-import AddTask from "./components/AddTask";
 import Footer from "./components/Footer";
 
 export default {
@@ -29,83 +19,20 @@ export default {
 
   components: {
     Header,
-    Tasks,
-    AddTask,
     Footer,
+    // on every page
   },
   // data 维持初始化组件时组件的状态
   data: function () {
     return {
-      tasks: [],
       showAddTask: false,
     };
   },
   methods: {
-    async deleteTask(id) {
-      if (confirm("Arey you sure?")) {
-        // 过滤掉 task.id 为 id 的 task
-        const res = await fetch(`api/tasks/${id}`, {
-          method: "DELETE",
-        });
-        res.status === 200
-          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
-          : alert("Error deleting task");
-      }
-    },
-    async toggleReminder(id) {
-      const taskToToggle = await this.fetchTask(id);
-      const updateTask = {
-        ...taskToToggle,
-        reminder: !taskToToggle.reminder,
-      };
-      const res = await fetch(`api/tasks/${id}`, {
-        method: "PUT",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(updateTask),
-      });
-
-      const data = await res.json();
-
-      console.log(`toggle task ${id}`);
-      this.tasks = this.tasks.map((task) =>
-        task.id === id ? { ...task, reminder: data.reminder } : task
-      );
-    },
-    async addTask(task) {
-      console.log("add Task");
-      const res = await fetch("api/tasks", {
-        method: "POST",
-        headers: {
-          "Content-type": "application/json",
-        },
-        body: JSON.stringify(task),
-      });
-      const data = await res.json();
-
-      this.tasks = [...this.tasks, data];
-    },
-
     toggleAddTask() {
       console.log("toggleAddTask");
       this.showAddTask = !this.showAddTask;
     },
-    async fetchTasks() {
-      const res = await fetch("api/tasks");
-      const data = await res.json();
-      return data;
-    },
-    async fetchTask(id) {
-      const res = await fetch(`api/tasks/${id}`);
-      const data = await res.json();
-      return data;
-    },
-  },
-
-  async created() {
-    // 创建一些硬编码的信息
-    this.tasks = await this.fetchTasks();
   },
 };
 </script>
---
+++ src/views/Home.vue
@@ -0,0 +1,92 @@
+<template>
+  <div v-if="showAddTask">
+    <AddTask @add-task="addTask" />
+  </div>
+  <Tasks
+    @toggle-reminder="toggleReminder"
+    @delete-task="deleteTask"
+    :tasks="tasks"
+  />
+</template>
+
+<script>
+import Tasks from "../components/Tasks";
+import AddTask from "../components/AddTask";
+
+export default {
+  name: "Home",
+  props: {
+    showAddTask: Boolean,
+  },
+  components: {
+    Tasks,
+    AddTask,
+  },
+  data() {
+    return { tasks: [] };
+  },
+  methods: {
+    async deleteTask(id) {
+      if (confirm("Arey you sure?")) {
+        // 过滤掉 task.id 为 id 的 task
+        const res = await fetch(`api/tasks/${id}`, {
+          method: "DELETE",
+        });
+        res.status === 200
+          ? (this.tasks = this.tasks.filter((task) => task.id !== id))
+          : alert("Error deleting task");
+      }
+    },
+    async toggleReminder(id) {
+      const taskToToggle = await this.fetchTask(id);
+      const updateTask = {
+        ...taskToToggle,
+        reminder: !taskToToggle.reminder,
+      };
+      const res = await fetch(`api/tasks/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(updateTask),
+      });
+
+      const data = await res.json();
+
+      console.log(`toggle task ${id}`);
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: data.reminder } : task
+      );
+    },
+    async addTask(task) {
+      console.log("add Task");
+      const res = await fetch("api/tasks", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(task),
+      });
+      const data = await res.json();
+
+      this.tasks = [...this.tasks, data];
+    },
+
+    async fetchTasks() {
+      const res = await fetch("api/tasks");
+      const data = await res.json();
+      return data;
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`);
+      const data = await res.json();
+      return data;
+    },
+  },
+
+  async created() {
+    // 创建一些硬编码的信息
+    this.tasks = await this.fetchTasks();
+  },
+};
+</script>
